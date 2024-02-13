339ea38df105cfd3cd12867a3f6aff15735e483e
shenoyts <159753320+shenoyts@users.noreply.github.com>
2024-02-13 22:16:59 +0530

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
-If streamlit.button('Get fruit load list'):
+if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows= get_fruit_load_list()
 streamlit.header("The fruit load list contains: ")
